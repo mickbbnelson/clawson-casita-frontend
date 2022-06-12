6afc1ac8b7de4694a72e8131a9384f079990c513
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-06-11 22:41:24 -0600

    Update App background-color

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,19 @@
+import styled from 'styled-components'
+
+const Styles = styled.div`
+
+    .aboutDiv {
+        background-color:  #C2B280;
+    }
+`;
+
 const About = () => {
     return(
-        <>
+        <Styles>
+        <div className='aboutDiv'>
             <h1>About</h1>
-        </>
+        </div>
+        </Styles>
     )
 }
 
