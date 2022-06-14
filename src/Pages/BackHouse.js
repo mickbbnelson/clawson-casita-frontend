ccbcd1969d6299709000e2108e0e7cecfffc038d
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-06-13 22:29:10 -0600

    Add fullcalendar gem and calendar component

--- src/Pages/BackHouse.js
+++ src/Pages/BackHouse.js
@@ -1,7 +1,12 @@
+import Calendar from "../Components/Calendar"
+
 const BackHouse = () => {
     return(
         <>
             <h1>Back House</h1>
+            <div className="calendar">
+                <Calendar />
+            </div>
         </>
     )
 }
--- src/Pages/FrontHouse.js
+++ src/Pages/FrontHouse.js
@@ -1,7 +1,12 @@
+import Calendar from "../Components/Calendar"
+
 const FrontHouse = () => {
     return(
         <>
             <h1>Front House</h1>
+            <div className="calendar">
+                <Calendar />
+            </div>
         </>
     )
 }
